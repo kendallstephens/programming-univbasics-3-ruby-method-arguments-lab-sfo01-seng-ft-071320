41fefa747586db536416b111056245aa63c46e70
Kendall Stephens <kendoll.stephens@gmail.com>
2020-06-25 14:11:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, language)
+def introduction_with_language(name = Dan, language = Ember)
   puts "Hi, my name is #{name} and I am learning to program in {language}."
 end
 
